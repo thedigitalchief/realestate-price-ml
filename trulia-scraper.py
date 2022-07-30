624122344f4a7597d697f4641897168c219c845a
thedigitalchief <me@dylanhnguyen.com>
2022-07-29 18:04:16 -0700

    added required header for scraping request

--- trulia-scraper.py
+++ trulia-scraper.py
@@ -4,3 +4,13 @@ import numpy as np
 import pandas as pd
 import regex as re
 import requests
+
+
+# HTTP header for scraping requests:
+req_headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.8',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Chrome/61.0.3163.100 Safari/537.36'
+}
