2729eefdd4d451592357d9b58ce35c69c0acb557
thedigitalchief <me@dylanhnguyen.com>
2022-07-29 18:11:14 -0700

    set up sql database and queried

--- data-cleaner.py
+++ data-cleaner.py
@@ -3,3 +3,11 @@ import sqlite3
 import regex as re
 from mlxtend.preprocessing import minmax_scaling
 #from sklearn.model_selection import train_test_split
+
+
+
+#query the raw trulia house data from SQLite database
+conn = sqlite3.connect(
+    '/Users/dylannguyen/Documents/Coding/trulia_house_data.db')
+
+houses_dataframe = pd.read_sql_query("SELECT * FROM trulia_house_raw_data", conn)
