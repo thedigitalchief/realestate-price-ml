3c93975119c575a7f757830e361485e0f3cd4c43
thedigitalchief <me@dylanhnguyen.com>
2022-08-03 17:04:08 -0700

    converting house built date into house age value

--- data-cleaner.py
+++ data-cleaner.py
@@ -5,7 +5,7 @@ from mlxtend.preprocessing import minmax_scaling
 #from sklearn.model_selection import train_test_split
 
 
-
+#takes ~1-2 min per listing
 #query the raw trulia house data from SQLite database
 conn = sqlite3.connect(
     '/Users/dylannguyen/Documents/Coding/trulia_house_data.db')
@@ -13,10 +13,13 @@ conn = sqlite3.connect(
 houses_dataframe = pd.read_sql_query("SELECT * FROM trulia_house_raw_data", conn)
 
 
+#congregating missing values
 missing_values_count = houses_dataframe.isnull().sum()
-
 print('___RAW DATA SUMMARY____')
-
 print(missing_values_count)
-
 print("number of samples: "+str(len(houses_dataframe)))
+
+
+#converting "year built" value into "house age"
+houses_dataframe['house_age'] = houses_dataframe['year_built'].apply(
+    lambda x: 2022-x)
